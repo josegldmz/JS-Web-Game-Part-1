65ec51a87ba44018e4c2464b5cc26c1ff3529a00
Jose <gldmz0318@icloud.com>
2022-11-16 19:44:56 -0800

    Fixed newImage and newObject

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage (url, left, bottom) {
+    let item = document.createElement('img')
+    item.src = url
+    item.style.position = 'fixed'
+    item.style.left = left + 'px'
+    item.style.bottom = bottom + 'px'
+    document.body.append(item)
+}
+
+function newObject(url, left, bottom){
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
+
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newObject('assets/sword.png', 500, 405)
+newObject('assets/sheild.png', 165, 185)
+newObject('assets/staff.png', 600, 100)
+
